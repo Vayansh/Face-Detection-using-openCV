52a757e3d2668f8f4c83228d4c524545569ac659
Vayansh Garg <vayanshgarg@gmail.com>
2022-07-05 17:33:27 +0530

    Face_box_detection
    Used haar-cascedes of openCV and Basics opencv and made face detection box real time

---
+++ faces.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+cam = cv.VideoCapture(0)
+
+face_cascade = cv.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+    frame = cv.flip(frame,1)
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,scaleFactor = 1.5,minNeighbors = 4)
+    for (x,y,w,h) in faces:
+        
+        #ROI
+        roi = gray[y:y+h ,x:x+w]
+        
+        cv.imwrite('ROI.png',roi)
+        
+        
+        pt = (x,y)
+        pt3 = (x+w,y+h)    
+        frame = cv.rectangle(frame,pt,pt3,(255,0,0),2)
+        # cv.imshow('frame',frame)
+    cv.imshow('frame',frame)
+        
+    key = cv.waitKey(1)
+    if key == ord('q') or key%256 == 27:
+        break
+    
+cam.release()    
+cv.destroyAllWindows()    
